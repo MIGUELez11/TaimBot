95164de9ca2835c32bf5e6083fd0dc33ad8adb07
BuildTools <miguel.mangel.ez11@gmail.com>
2018-09-23 16:41:01 +0200

    Version 1.0

---
+++ TaimBot/Modules/Ping.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutorialBot.Modules
+{
+    public class Ping : ModuleBase<SocketCommandContext>
+    {
+        [Command("Hola")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync("Hello World");
+        }
+    }
+}
