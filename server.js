7a1e9dc6961d3298b206926b899e4a6c15d16a6d
MIGUELez11 <miguel.mangel.ez11@gmail.com>
2018-09-26 18:54:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -55,22 +55,6 @@ var BgWhite = "\x1b[47m";
 const youtube = new YouTube(GOOGLE_API_KEY);
 const queue = new Map();
 
-
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-
-
-
 /*
 ------------------------------------------------------
                         FUNCTIONS
