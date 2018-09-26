85cba699f9f8bf99f33c525986c2bb20d52438c2
MIGUELez11 <miguel.mangel.ez11@gmail.com>
2018-09-26 07:39:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-exports.TOKEN = 'NDkzMzkzMDc0MTUyNTM4MTEz.Dok_aA.Y2ndOJHDYKOgmt6Zhfdwd5THLsY';
+exports.TOKEN = '';
 
 exports.PREFIX = '/';
 
-exports.GOOGLE_API_KEY = 'AIzaSyADcwBTTJp-7QaFkXM8E0mSUP_EMuR88DA';
+exports.GOOGLE_API_KEY = '';
 
 exports.DEFAULTROLE = 'Taimers';
