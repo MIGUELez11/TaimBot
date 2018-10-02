0014d2d8d1d9ba24ab68f3046e754a779879d9ea
BuildTools <miguel.mangel.ez11@gmail.com>
2018-10-03 00:27:20 +0200

    12

--- server.js
+++ server.js
@@ -379,7 +379,7 @@ if ((h >= minH && h <= maxH)) {
                      name: "Música",
                      value: `
                      /music [estos comandos funcionan en español]
-                        play (nombre canción)  -> reproduce música en el canal de voz.
+                     ___play (nombre canción)  -> reproduce música en el canal de voz.
                         pause                  -> pausa la canción.
                         resume                 -> reanuda la canción.
                         stop                   -> Para el reproductor.
